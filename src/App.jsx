eff4a47dae9db827b163922c8920108ba89aaa53
faizanasif728 <fasif4832@gmail.com>
2024-09-01 11:18:21 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+import Comp2 from "./components/Comp2";
+function App() {
+    return (
+    <>
+        <Counter/>
+        <Comp2/>    
+    </>
+  )
+}
+
+export default App;
